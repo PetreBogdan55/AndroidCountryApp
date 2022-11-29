e5be78c074ed72f1c464a0fcf7fa1bd7d86d4166
Petre Bogdan <petrebogdan55@yahoo.com>
2022-11-29 16:40:31 +0200

    Added first fragment

--- app/src/main/java/com/example/countryapp/MainActivity.java
+++ app/src/main/java/com/example/countryapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.countryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CountryListFragment countryListFragment=new CountryListFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.main_frame_layout, countryListFragment);
+        fragmentTransaction.commit();
     }
 }
